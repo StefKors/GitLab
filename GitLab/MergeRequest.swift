f5133df32fec0ee84ba700f7240a90815be61da7
Stef Kors <stef.kors@gmail.com>
2021-09-13 20:01:33 +0100

    Update MergeRequest.swift

--- GitLab/MergeRequest.swift
+++ GitLab/MergeRequest.swift
@@ -15,38 +15,55 @@ import Foundation
 // MARK: - MergeRequestElement
 struct MergeRequestElement {
     let id, iid, projectID: Int
-    let title, mergeRequestDescription, state: String
-    let mergedBy: Assignee?
-    let mergedAt, closedBy, closedAt: String
-    let createdAt, updatedAt: Date
-    let targetBranch, sourceBranch: String
-    let upvotes, downvotes: Int
-    let author, assignee: Assignee
-    let assignees, reviewers: [Assignee]
+    let title, mergeRequestDescription: String
+    let state: MergeRequestState
+    let createdAt, updatedAt: String
+    let mergedBy: Author?
+    let mergedAt, closedBy, closedAt: String?
+    let targetBranch: TargetBranch
+    let sourceBranch: String
+    let userNotesCount, upvotes, downvotes: Int
+    let author: Author
+    let assignees: [Author]
+    let assignee: Author?
+    let reviewers: [Author]
     let sourceProjectID, targetProjectID: Int
     let labels: [String]
     let draft, workInProgress: Bool
-    let milestone: Milestone
+    let milestone: Milestone?
     let mergeWhenPipelineSucceeds: Bool
-    let mergeStatus, sha: String
-    let mergeCommitSHA, squashCommitSHA: String?
-    let userNotesCount: Int
-    let discussionLocked: Bool?
-    let shouldRemoveSourceBranch, forceRemoveSourceBranch, allowCollaboration, allowMaintainerToPush: Bool
-    let webURL: String
+    let mergeStatus: MergeStatus
+    let sha: String
+    let mergeCommitSHA, squashCommitSHA, discussionLocked: NSNull
+    let shouldRemoveSourceBranch: Bool?
+    let forceRemoveSourceBranch: Bool
+    let reference: String?
     let references: References
+    let webURL: String
     let timeStats: TimeStats
     let squash: Bool
     let taskCompletionStatus: TaskCompletionStatus
     let hasConflicts, blockingDiscussionsResolved: Bool
+    let approvalsBeforeMerge: NSNull?
+    let allowCollaboration, allowMaintainerToPush: Bool?
 }
 
-// MARK: - Assignee
-struct Assignee {
+// MARK: - Author
+struct Author {
     let id: Int
-    let name, username, state: String
+    let name, username: String
+    let state: AuthorState
     let avatarURL: String?
     let webURL: String
+}
+
+enum AuthorState {
+    case active
+}
+
+enum MergeStatus {
+    case canBeMerged
+    case unchecked
 }
 
 // MARK: - Milestone
@@ -62,6 +79,16 @@ struct References {
     let short, relative, full: String
 }
 
+enum MergeRequestState {
+    case merged
+    case opened
+}
+
+enum TargetBranch {
+    case develop
+    case master
+}
+
 // MARK: - TaskCompletionStatus
 struct TaskCompletionStatus {
     let count, completedCount: Int
@@ -72,4 +99,3 @@ struct TimeStats {
     let timeEstimate, totalTimeSpent: Int
     let humanTimeEstimate, humanTotalTimeSpent: NSNull
 }
-
