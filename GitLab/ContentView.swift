feee69a2135ddf2b25251b97be738e9980ee3d65
Stef Kors <stef.kors@gmail.com>
2021-09-13 19:31:44 +0100

    Initial Commit

---
+++ GitLab/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  GitLab
+//
+//  Created by Stef Kors on 13/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
