d5b1a4ae3ee513921e69906f01046a1b3d9b2fac
Stef Kors <stef.kors@gmail.com>
2021-09-14 14:36:05 +0100

    save

--- GitLab/AppDelegate.swift
+++ GitLab/AppDelegate.swift
@@ -10,30 +10,30 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     var popover: NSPopover!
     var statusBarItem: NSStatusItem!
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
-
+        
         // Create the popover
         let popover = NSPopover()
         popover.contentSize = NSSize(width: 400, height: 500)
         popover.behavior = .transient
         popover.contentViewController = NSHostingController(rootView: contentView)
         self.popover = popover
-
+        
         // Create the status item
         self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
-
+        
         if let button = self.statusBarItem.button {
             button.image = NSImage(named: "Icon")
             button.action = #selector(togglePopover(_:))
         }
     }
-
+    
     @objc func togglePopover(_ sender: AnyObject?) {
         if let button = self.statusBarItem.button {
             if self.popover.isShown {
@@ -43,5 +43,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
-
+    
 }
--- GitLab/NetworkManager.swift
+++ GitLab/NetworkManager.swift
@@ -8,30 +8,56 @@
 import Foundation
 import Combine
 
+enum RequestError: Error {
+    case sessionError(error: Error)
+}
+
 class NetworkManager: ObservableObject {
     // https://gitlab.com/api/v4/projects/22103425/merge_requests?state=opened
     var url: URL = URL(string: "https://www.gitlab.com/api/v4/projects/22103425/merge_requests?state=opened")!
     var token: String = "Bearer DYjsR1sjWmsPBwBMdipb"
+    var lastUpdate: Date = NSDate.now
 //    @Published var dataTask
-    @Published var mergeRequests: [MergeRequestElement] = []
+    @Published var mergeRequests: [MergeRequestElement] = Mock.MRs {
+        didSet {
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(2000)) {
+                self.isUpdatingMRs = false
+            }
+
+        }
+    }
+    @Published var isUpdatingMRs: Bool = false
     private var scope: Set<AnyCancellable> = []
 
-    func name() {
+    func getMRs() {
+        isUpdatingMRs = true
+
         // network call
         let sessionConfiguration = URLSessionConfiguration.default
         sessionConfiguration.httpAdditionalHeaders = ["Authorization": token]
 
-        let session = URLSession(configuration: sessionConfiguration)
-        session.dataTaskPublisher(for: url)
-            .map({ item in
-                return item.data
-            })
-            .decode(type: [MergeRequestElement].self, decoder: JSONDecoder())
-            .replaceError(with: [])
-            .receive(on: RunLoop.main)
-            .assign(to: \.mergeRequests, on: self)
-            .store(in: &scope)
+//        let session = URLSession(configuration: sessionConfiguration)
+//        session.dataTaskPublisher(for: url)
+//            .map({ item in
+//                return item.data
+//            })
+//            .decode(type: [MergeRequestElement].self, decoder: JSONDecoder())
+//            .catch { error -> AnyPublisher<[MergeRequestElement], Never> in
+//                print(error)
+//                if error is URLError {
+//                    return Just([])
+//                        .eraseToAnyPublisher()
+//                } else {
+//                    return Empty(completeImmediately: true)
+//                        .eraseToAnyPublisher()
+//                }
+//            }
+//            .retry(3)
+////            .replaceError(with: [])
+//            .receive(on: RunLoop.main)
+//            .assign(to: \.mergeRequests, on: self)
+//            .store(in: &scope)
 
-        print(scope)
+        mergeRequests = Mock.MRs
     }
 }
