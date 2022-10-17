1f05526c12207b668f408b8195c4f32f3d6bc238
Stef Kors <stef@beamapp.co>
2022-10-17 15:07:05 +0200

    cleanup placeholder view so it doesn't look like a scrollbar

--- UserInterface/Sources/UserInterface/Views/LastUpdateMessagePlaceholderView.swift
+++ UserInterface/Sources/UserInterface/Views/LastUpdateMessagePlaceholderView.swift
@@ -8,24 +8,14 @@
 import SwiftUI
 
 struct LastUpdateMessagePlaceholderView: View {
-    @State var placeholderColor: Color = .secondary
-
     var body: some View {
         Text("Last updated at: 19 September 2022 at 12:43")
+            .opacity(0)
             .redacted(reason: .placeholder)
-            .foregroundColor(placeholderColor)
-            .animation(
-                .linear(duration: 0.3)
-                .delay(0.35)
-                .repeatForever(autoreverses: true),
-                value: placeholderColor)
             .transition(.opacity.animation(.easeInOut(duration: 0.35).delay(0.2)))
             .foregroundColor(.gray)
             .font(.system(size: 10))
             .cornerRadius(5)
-            .onAppear {
-                placeholderColor = .primary
-            }
     }
 }
 
