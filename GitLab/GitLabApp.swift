bdab85070267396c5341327ff1bf3595e4dacf24
Stef Kors <stef.kors@gmail.com>
2023-08-19 17:07:37 +0200

    Update GitLabApp.swift

--- GitLab/GitLabApp.swift
+++ GitLab/GitLabApp.swift
@@ -8,6 +8,29 @@
 import SwiftUI
 import SwiftData
 
+// struct AnalogTimerView: View {
+//     let date: Date
+//     let showSeconds: Bool
+//     var formatter: DateFormatter {
+//         let formatter = DateFormatter()
+//         formatter.dateFormat = "HH:mm:ss:SS"
+//         return formatter
+//     }
+//     var body: some View {
+//         Text(date, formatter: formatter)
+//         // .font(.largeTitle)
+//             .fontDesign(.monospaced)
+//             .contentTransition(.numericText(value: date.timeIntervalSinceReferenceDate))
+//             .animation(.bouncy(duration: 0.1), value: date)
+//     }
+// }
+
+// TimelineView(.periodic(from: startDate, by: 0.01)) { context in
+//     AnalogTimerView(
+//         date: context.date,
+//         showSeconds: context.cadence <= .seconds)
+// }
+
 @main
 struct GitLabApp: App {
     @StateObject var networkManager = NetworkManager()
