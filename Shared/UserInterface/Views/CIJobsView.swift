0d016ff167ff3f54820ce5866d1cddd1d777360f
Matthew Fisher <92025456+MatthewTHFisher@users.noreply.github.com>
2024-10-07 15:58:28 +0100

    Correct file line spacing

--- Shared/UserInterface/Views/CIJobsView.swift
+++ Shared/UserInterface/Views/CIJobsView.swift
@@ -26,29 +26,30 @@ public struct CIJobsView: View {
                 presentPopover.toggle()
             }, label: {
                 CIStatusView(status: stage.status?.toPipelineStatus())
-            }).buttonStyle(.borderless)
-                .popover(isPresented: $presentPopover, content: {
-                    if let jobs = stage.jobs?.edges?.map({ $0.node }) {
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text(stage.name?.capitalized ?? "")
-                                .fontWeight(.bold)
-                                .padding(.bottom, 4)
-                            ForEach(jobs.indices, id: \.self) { index in
-                                if let job = jobs[index] {
-                                    HStack {
-                                        if let path = job.detailedStatus?.detailsPath,
-                                           let destination = URL(string: model.$baseURL.wrappedValue + path) {
-                                            Link(destination: destination, label: {
-                                                CIStatusView(status: job.status)
-                                                Text(job.name ?? "")
-                                            })
-                                        }
+            })
+            .buttonStyle(.borderless)
+            .popover(isPresented: $presentPopover, content: {
+                if let jobs = stage.jobs?.edges?.map({ $0.node }) {
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(stage.name?.capitalized ?? "")
+                            .fontWeight(.bold)
+                            .padding(.bottom, 4)
+                        ForEach(jobs.indices, id: \.self) { index in
+                            if let job = jobs[index] {
+                                HStack {
+                                    if let path = job.detailedStatus?.detailsPath,
+                                       let destination = URL(string: model.$baseURL.wrappedValue + path) {
+                                        Link(destination: destination, label: {
+                                            CIStatusView(status: job.status)
+                                            Text(job.name ?? "")
+                                        })
                                     }
                                 }
                             }
-                        }.padding()
-                    }
-                })
+                        }
+                    }.padding()
+                }
+            })
         }
         .animation(.spring(response: 0.35, dampingFraction: 1, blendDuration: 0), value: isHovering)
         // .onHover { hovering in
